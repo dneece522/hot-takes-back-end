75b93a9f4d6ac6c803a4f0cb29c9830cac5941de
Drew Neece <dneece5@aggienetwork.com>
2023-07-21 17:06:10 -0500

    add takes to profile schema

--- models/profile.js
+++ models/profile.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
   name: String,
-  photo: String
+  photo: String,
+  takes: [{ type: Schema.Types.ObjectId, ref: 'Take' }]
 },{
   timestamps: true,
 })
